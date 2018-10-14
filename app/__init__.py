ffe4f2832f022642e0ab234685d49f04cfcf3ba2
fightfastest <fightfastest@github.com>
2018-10-15 07:43:19 +0800

    lixg

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 
 from flask import Flask
+from app.models.book import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.secure')
     app.config.from_object('app.setting')
     register_blueprint(app)
+
+    db.init_app(app)
+    db.create_all(app=app)
     return app
 
 def register_blueprint(app):
--- app/secure.py
+++ app/secure.py
@@ -1,3 +1,5 @@
 # 这是配置文件
 
 DEBUG = False
+
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/fish'
